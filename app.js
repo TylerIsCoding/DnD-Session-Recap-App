160541c6e0f573c936e8e1a51483de284441ef40
Tyler McKenna <tmckenna1988@gmail.com>
2023-04-04 12:14:25 -0400

    Added files

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require('express');
 const dotenv = require('dotenv');
 const PORT = process.env.PORT || 3000;
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 const app = express();
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Routes
+app.use('/', require('./routes/index'));
 
 connectDB();
 
