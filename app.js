c23c07477ed3cd719ade5c1d2de546cf2273dbde
Tyler McKenna <tmckenna1988@gmail.com>
2023-04-21 14:20:05 -0400

    Redownloaded dependencies and updated app.js

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const PORT = process.env.PORT || 3000;
-const morgan = require('morgan');
-const exphbs = require('express-handlebars');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 5000;
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const connectDB = require("./config/db");
 const app = express();
 
 // Load config
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
 }
 
 // Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
-app.set('view engine', '.hbs');
+
+// Will throw error if .engine is not after exphbs
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 // Routes
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    );
 });
