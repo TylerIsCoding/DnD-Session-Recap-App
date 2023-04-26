3abdb65f88fca2202b6b5f96facb197cfe763151
Tyler McKenna <tmckenna1988@gmail.com>
2023-04-26 16:38:16 -0400

    Added many changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const PORT = process.env.PORT || 5000;
@@ -19,6 +20,9 @@ if (process.env.NODE_ENV === "development") {
 // Will throw error if .engine is not after exphbs
 app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
