275ea07769651001edeea28a1c60e56c2d383972
Tyler McKenna <tmckenna1988@gmail.com>
2023-05-26 20:02:27 -0400

    DRY applied to dice rolling app

--- public/js/dm.js
+++ public/js/dm.js
@@ -5,11 +5,32 @@ const addD4Button = document.getElementById('add-d4');
 const subD4Button = document.getElementById('sub-d4');
 const d4Holder = document.getElementById('d4-holder');
 
-const rollAll = document.getElementById('roll');
+// D6
+const addD6Button = document.getElementById('add-d6');
+const subD6Button = document.getElementById('sub-d6');
+const d6Holder = document.getElementById('d6-holder');
 
-addD4Button.addEventListener('click', addD4)
-subD4Button.addEventListener('click', removeD4)
-rollAll.addEventListener('click', rollDice)
+// D8
+const addD8Button = document.getElementById('add-d8');
+const subD8Button = document.getElementById('sub-d8');
+const d8Holder = document.getElementById('d8-holder');
+
+// D10
+const addD10Button = document.getElementById('add-d10');
+const subD10Button = document.getElementById('sub-d10');
+const d10Holder = document.getElementById('d10-holder');
+
+// D12
+const addD12Button = document.getElementById('add-d12');
+const subD12Button = document.getElementById('sub-d12');
+const d12Holder = document.getElementById('d12-holder');
+
+// D20
+const addD20Button = document.getElementById('add-d20');
+const subD202Button = document.getElementById('sub-d20');
+const d20Holder = document.getElementById('d20-holder');
+
+const rollAll = document.getElementById('roll');
 
 class Bag {
     constructor() {
@@ -19,9 +40,10 @@ class Bag {
         let total = 0;
         this.dice.forEach(dice => {
             dice.roll()
+            console.log(dice.value())
             total += dice.value()
         })
-        console.log(total)
+        return total;
     }
 }
 
@@ -40,27 +62,42 @@ class Dice {
 
 const bag = new Bag()
 
-function addD4() {
-    const newD4 = new Dice(4);
-    bag.dice.push(newD4)
-    console.log('D4 added to bag!')
+function addDice(sides, holder) {
+    const newDice = new Dice(sides);
+    bag.dice.push(newDice)
+    console.log(`New D${sides} added to bag.`)
     let count = 0;
     bag.dice.forEach(dice => {
         if (dice.numOfFaces === 4) count++
     })
-    d4Holder.innerHTML = count;
+    holder.innerHTML = count;
 }
 
-function removeD4() {
+function removeDice(sides, holder) {
     for (let i = bag.dice.length - 1; i >= 0; i--) {
-       if (bag.dice[i].numOfFaces === 4) {
+       if (bag.dice[i].numOfFaces === sides) {
         bag.dice.pop();
         break;
        }
     }
-    d4Holder.innerHTML = bag.dice.length === 0 ? '' : bag.dice.length;
+    holder.innerHTML = bag.dice.length === 0 ? '' : bag.dice.length;
 }
 
-function rollDice() {
-    results.innerHTML = bag.rollAll();
-}
+let addArray = [addD4Button, addD6Button, addD8Button, addD10Button, addD12Button, addD20Button]
+let subArray = [subD4Button, subD6Button, subD8Button, subD10Button, subD12Button, subD202Button]
+
+rollAll.addEventListener('click', () => {
+    const roll = bag.rollAll();
+    results.innerHTML = roll === 0 ? '' : roll;
+})
+
+addArray.forEach(button => {
+    button.addEventListener('click', () => {
+    addDice(4, d4Holder)})
+})
+
+subArray.forEach(button => {
+   button.addEventListener('click', () => {
+    removeDice(4, d4Holder);
+   }) 
+})
